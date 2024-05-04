428beb1050f7ae6be1510e5755ec55a0993db205
Rick Bosch <emerick.bosch@gmail.com>
2024-05-05 01:04:44 +0200

    Try get styling to work

--- src/index-ux.tsx
+++ src/index-ux.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+// styles not working
+import "./styles.css"
 
-function TicTacToe() {
-  return <h1>Hello from React!</h1>;
+export const TicTacToe: React.FC = () => {
+    const [boardState, setBoardState] = useState<(typeof O | typeof X |undefined)[]>(Array(9).fill(undefined))
+
+    return <div className="container">
+    <div className="board">
+    {
+        boardState.map((value, index) => {
+            return <button key={index} className="gridTile" disabled={value !== undefined}>{valueToLabel(value)}</button>
+        })
+    }
+    </div>
+    </div>
 }
+
+const valueToLabel = (value?: typeof X | typeof O): string => {
+    switch(value) {
+        case undefined:
+            return ""
+        case X:
+            return "X"
+        case O:
+            return "O"
+    }
+}
+
+const X = 1
+const O = 0
 
 const domNode = document.getElementById('tic-tac-toe');
 if(domNode) {
